77eff01dd79ec45b6b31753eedd7770c776be0c9
Indraneel Patil <indraneelrajeevpatil@gmail.com>
2025-11-02 13:48:06 -0800

    Added script

---
+++ neel/scripts/lerobot_teleoperate.py
@@ -0,0 +1,18 @@
+"""
+Created by Indraneel on 11/02/2025
+
+Teleoperate Lerobot
+
+"""
+from lerobot.utils.utils import init_logging
+
+def teleoperate():
+    print("Starting teleoperate")
+    init_logging()
+
+
+def main():
+    teleoperate()
+
+if __name__ == "__main__":
+    main()
